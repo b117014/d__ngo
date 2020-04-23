bc1b5fddb3a0abba1198079f49994f838d2fa605
b117014 <b117014@iiit-bh.ac.in>
2020-04-23 23:24:25 +0530

    other routes

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)    # __name__ represent the current file
+
+@app.route("/")          # after the default routes open the browser just below function is called
+def index():
+    return "Hi there"
+
+@app.route("/hello")
+def hello():
+    return "hello"
+
+@app.route("/name/<string:name>")      # <string:name> denotes the name is variable that can put on the route and use in function
+def name_(name):
+    return f"hello {name}"
--- application.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)    # __name__ represent the current file
-
-@app.route("/")          # after the default routes open the browser just below function is called
-def index():
-    return "Hi there"
