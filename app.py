7663683d1b0c237d6e3c0be69f628095e45c90bd
b117014 <b117014@iiit-bh.ac.in>
2020-04-28 02:03:49 +0530

    session of individual user

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,session
+from flask_session import Session
 import datetime
 
 app = Flask(__name__)    # __name__ represent the current file
+
+# for server side render
+app.config["SESSION_PERMANENT"] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 @app.route("/")          # after the default routes open the browser just below function is called
 def index():
@@ -20,3 +26,12 @@ def newYear():
     date = datetime.datetime.now();
     new_year = date.day == 1 and date.month==1
     return render_template('first.html',new_year=new_year,date=date)
+
+@app.route('/home',methods=["POST"])              # post route in flask
+def home():
+    session['notes'] = []
+    name = request.form.get('home')            # request.form.get() method to request the input data and works similar to req.body
+    session['notes'].append(name)
+    print(name)
+    return render_template('index.html',name=name)
+
