fe424e9fe052af36d769148a68300f433a3022d8
b117014 <b117014@iiit-bh.ac.in>
2020-05-02 01:34:56 +0530

    syntax of inserting new row in table

--- sql/list.py
+++ sql/list.py
@@ -7,3 +7,9 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 engine = create_engine(os.getenv("DATABASE_URL"))
 # scoped_session create different session for every user  
 db = scoped_session(sessionmaker(bind=engine))  
+
+
+def main():
+    flights = db.execute("SELECT origin,destination,duration FROM fligts").findall();  # fetchall to get me all the data
+    for flight in flights:
+        print(f"{flight.origin} to {flight.destination} hour {flight.duration}")
