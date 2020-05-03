20131736d502127b3a86e8e649e06a3117210cc6
root <prabhatkmr5789@gmail.com>
2020-05-03 23:40:50 +0530

    using flaskAslschemy

---
+++ model/app.py
@@ -0,0 +1,30 @@
+import os
+import csv;
+from model import *
+from flask import render_template,request,Flask
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://prabhatku:1234@localhost/airport"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)   # Tie up Database with flask application
+
+def main():
+    db.create_all()    # create database
+    file = open('./data/flights.csv')
+    data = csv.reader(file)
+    for origin,destination,duration in data:
+        flight = Flights(origin=origin,destination=destination,duration=duration)
+        db.session.add(flight)
+    db.session.commit()
+
+    
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        main()
+
+
+# SELECT * FROM fligts    ===    Fligts.query.all()
+#  SELECT * FROM flights WHERE origin = "Paris"    ===   Flights.query.filter_by(origin="Paris").all()
--- model/model.py
+++ model/model.py
@@ -1,17 +1,16 @@
-from flask_sqlalchemy import SQLAlchemy;
-
+from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
-class Fligts(db.model):        # db.model means flihts class inherits the db class
-    __tablename__ = 'fligts'
+class Flights(db.Model):        # db.model means flihts class inherits the db class
+    __tablename__ = 'flights'     # to define the table name
     id = db.Column(db.Integer,primary_key=True)
     origin = db.Column(db.String, nullable=False)
     destination = db.Column(db.String, nullable=False)
     duration = db.Column(db.Integer, nullable=False)
 
-class Passangers(db.model):
+class Passangers(db.Model):
     __tablename__ = 'passangers'
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String,nullable=False)
-    flight_id = db.Column(db.Integer,db.ForeignKey('fligts.id'),nullable=False)     # adding foreign key
-    
+    flight_id = db.Column(db.Integer,db.ForeignKey('flights.id'),nullable=False)     # adding foreign key
+
